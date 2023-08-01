0a66c1391237152e3728087a53c5ddf75462a524
Sadie98 <avdeewa.ul@ya.ru>
2023-08-02 00:17:27 +0100

    Dragging added

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,19 +1,8 @@
 <script setup lang="ts">
-import { Column } from "~/types";
+import { Column, Task } from "~/types";
 import { nanoid } from "nanoid";
-
+import draggable from "vuedraggable";
 const columns = ref<Column[]>([
-  {
-    id: nanoid(),
-    title: "To Do",
-    tasks: [
-      {
-        id: nanoid(),
-        title: "Leave the company",
-        createdAt: new Date(),
-      },
-    ],
-  },
   {
     id: nanoid(),
     title: "To Do",
@@ -62,22 +51,39 @@ const columns = ref<Column[]>([
 </script>
 
 <template>
-  <div class="flex gap-4 overflow-x-auto items-start">
-    <div
-      v-for="column in columns"
-      :key="column.id"
-      class="column bg-gray-200 p-5 rounded min-w-[250px]"
+  <div>
+    <draggable
+      v-model="columns"
+      group="columns"
+      animation="150"
+      handle=".drag-handle"
+      item-key="id"
+      class="flex gap-4 overflow-x-auto items-start"
     >
-      <header>{{ column.title }}</header>
-      <TrelloBoardTask
-        v-for="task in column.tasks"
-        :task="task"
-        :key="task.id"
-      />
-      <footer>
-        <button class="text-gray-500">+ Add a card</button>
-      </footer>
-    </div>
+      <template #item="{ element: column }: { element: Column }">
+        <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
+          <header class="font-bold mb-4">
+            <DragHandler />
+            {{ column.title }}
+          </header>
+          <draggable
+            v-model="column.tasks"
+            group="tasks"
+            handle=".drag-handle"
+            animation="150"
+            item-key="id"
+          >
+            <template #item="{ element: task }: { element: Task }">
+              <TrelloBoardTask :task="task" />
+            </template>
+          </draggable>
+
+          <footer>
+            <button class="text-gray-500">+ Add a card</button>
+          </footer>
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
 
