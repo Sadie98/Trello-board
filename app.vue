532dc629f1b73ef51583d2022188e85b9b7fbf05
Sadie98 <avdeewa.ul@ya.ru>
2023-08-01 23:37:08 +0100

    Main board components

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">Trello board</h1>
+    <TrelloBoard />
   </div>
 </template>
+<script setup lang="ts">
+import TrelloBoard from "~/components/TrelloBoard.vue";
+</script>
---
+++ components/TrelloBoard.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { Column } from "~/types";
+import { nanoid } from "nanoid";
+
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: "To Do",
+    tasks: [
+      {
+        id: nanoid(),
+        title: "Leave the company",
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: "To Do",
+    tasks: [
+      {
+        id: nanoid(),
+        title: "Leave the company",
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: "In progress",
+    tasks: [
+      {
+        id: nanoid(),
+        title: "Find new job",
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: "In QA",
+    tasks: [
+      {
+        id: nanoid(),
+        title: "Feed the cat",
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: "Done",
+    tasks: [
+      {
+        id: nanoid(),
+        title: "Eat",
+        createdAt: new Date(),
+      },
+    ],
+  },
+]);
+</script>
+
+<template>
+  <div class="flex gap-4 overflow-x-auto items-start">
+    <div
+      v-for="column in columns"
+      :key="column.id"
+      class="column bg-gray-200 p-5 rounded min-w-[250px]"
+    >
+      <header>{{ column.title }}</header>
+      <TrelloBoardTask
+        v-for="task in column.tasks"
+        :task="task"
+        :key="task.id"
+      />
+      <footer>
+        <button class="text-gray-500">+ Add a card</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
